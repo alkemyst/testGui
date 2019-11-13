2fa92c6f745959e5134f82452131f21f4a4378fd
Stefano Mersi <stefano.mersi@cern.ch>
2019-11-13 17:04:44 +0100

    Adding a second form

--- src/testGui.py
+++ src/testGui.py
@@ -2,12 +2,15 @@ import sys
 from MainWindow import Ui_MainWindow
 from PySide2.QtWidgets import QApplication, QMainWindow
 
+def quit_func():
+    sys.exit(app.exec_())
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.ui.button_a.clicked.connect(quit_func)
 
         # use self.ui here...
 
